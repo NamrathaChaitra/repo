eec1a58f9ad1f4615e6a3cea7ca0bdf25842a2b5
HOME BASIC <HOME BASIC@192.168.0.3>
2017-07-16 15:38:45 +0530

    commited to git

---
+++ src/com/actitime/tests/MavenDemo.java
@@ -0,0 +1,13 @@
+package com.actitime.tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenDemo {
+@Test
+public void testA(){
+	Reporter.log("Hi",true);
+	
+}
+	
+}
